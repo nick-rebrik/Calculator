a25d57f3087442218e9cffb7020391af530cfc7b
nick-rebrik <77996995+nick-rebrik@users.noreply.github.com>
2021-08-18 19:55:48 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,5 @@
 import datetime as dt
 
-DATE_FORMAT = '%d.%m.%Y'
-
 
 class Calculator:
     def __init__(self, limit):
@@ -27,15 +25,16 @@ class Calculator:
 
 
 class Record:
+    DATE_FORMAT = '%d.%m.%Y'
 
     def __init__(self, amount, comment, date=None):
         self.amount = amount
         self.comment = comment
 
         if date is not None:
-            self.date = dt.datetime.strptime(date, DATE_FORMAT).date()
+            self.date = dt.datetime.strptime(date, self.DATE_FORMAT).date()
         else:
-            self.date = dt.datetime.now().date()
+            self.date = dt.date.today()
 
     def __str__(self):
         return f'{self.amount}, {self.comment}, {self.date}'
@@ -47,55 +46,35 @@ class CashCalculator(Calculator):
     RUB_RATE = 1
 
     def get_today_cash_remained(self, currency):
+        exchange = {
+            'rub': (self.RUB_RATE, 'руб'),
+            'usd': (self.USD_RATE, 'USD'),
+            'eur': (self.EURO_RATE, 'Euro'),
+        }
+
+        if currency not in exchange:
+            return 'Валюта не поддерживается'
+
         remainder = self.today_remained()
 
         if remainder == 0:
             return 'Денег нет, держись'
 
-        exchange = {
-            'rub': {
-                'rate': self.RUB_RATE,
-                'type': 'руб'
-            },
-            'usd': {
-                'rate': self.USD_RATE,
-                'type': 'USD'
-            },
-            'eur': {
-                'rate': self.EURO_RATE,
-                'type': 'Euro'
-            },
-        }
-
-        limit_rate = round(remainder / exchange[currency]['rate'], 2)
-        limit_type = exchange[currency]['type']
+        rate, limit_type = exchange[currency]
+        limit_rate = round(remainder / rate, 2)
 
         if limit_rate < 0:
             abs_limit_rate = abs(limit_rate)
             return (f'Денег нет, держись: твой долг - {abs_limit_rate} '
                     f'{limit_type}')
-
         return f'На сегодня осталось {limit_rate} {limit_type}'
 
 
 class CaloriesCalculator(Calculator):
-
     def get_calories_remained(self):
         remainder = self.today_remained()
 
         if remainder > 0:
             return ('Сегодня можно съесть что-нибудь ещё, но с общей '
                     f'калорийностью не более {remainder} кКал')
-
         return 'Хватит есть!'
-
-
-if __name__ == '__main__':
-    cash_calculator = CashCalculator(1000)
-    cash_calculator.add_record(Record(amount=145, comment='кофе'))
-    cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-    cash_calculator.add_record(Record(amount=3000,
-                                      comment='бар в Танин др',
-                                      date='08.11.2019'))
-
-    print(cash_calculator.get_today_cash_remained('rub'))
